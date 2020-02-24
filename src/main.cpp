dce5f1b931ed95625ddc5f91a2bac4507a0fe983
Anders Steen <anders.steen98@gmail.com>
2020-02-24 11:09:52 +0100

    complete chapter 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
-#include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
 
 using namespace std;
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  float a = dot(r.direction(), r.direction());
-  float b = 2.0 * dot(oc, r.direction());
-  float c = dot(oc, oc) - radius*radius;
-  float discriminant = b*b - 5*a*c;
-  return discriminant > 0;
-}
-
-vec3 color(const ray& r) {
-  if (hit_sphere(vec3(0,0,-1), 0.5, r)) {
-    return vec3(1,0,0);
+vec3 color(const ray& r, hitable* world) {
+  hit_record rec;
+  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+    return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+  } else {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
   }
-  vec3 unit_direction = unit_vector(r.direction());
-  float t = 0.5*(unit_direction.y() + 1.0);
-  return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
-  int nx = 200;
-  int ny = 100;
+  int nx = 1920;
+  int ny = 1080;
   cout << "P3\n" << nx << " " << ny << "\n255\n";
   vec3 lower_left_corner(-2.0, -1.0, -1.0);
   vec3 horizontal(4.0, 0.0, 0.0);
   vec3 vertical(0.0, 2.0, 0.0);
   vec3 origin(0.0, 0.0, 0.0);
+
+  hitable* list[2];
+  list[0] = new sphere(vec3(0,0,-1), 0.5);
+  list[1] = new sphere(vec3(0,-100.5,-1), 100);
+  hitable* world = new hitable_list(list, 2);
+
   for (int j = ny-1; j >= 0; --j) {
     for (int i = 0; i < nx; ++i) {
       float u = float(i) / float(nx);
       float v = float(j) / float(ny);
       ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-      vec3 col = color(r);
+
+      vec3 p = r.point_at_parameter(2.0);
+      vec3 col = color(r, world);
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
