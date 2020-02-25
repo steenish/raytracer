ad8ddad2a2142d7cc150b4d77017e757ad2e2430
Anders Steen <anders.steen98@gmail.com>
2020-02-25 01:33:24 +0100

    complete chapter 12

--- src/main.cpp
+++ src/main.cpp
@@ -33,19 +33,50 @@ hitable* tutorial_scene() {
   return new hitable_list(list, n);
 }
 
+hitable* random_scene() {
+  int n = 500;
+  hitable** list = new hitable*[n+1];
+  list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+  int i = 1;
+
+  for (int a = -11; a < 11; ++a) {
+    for (int b = -11; b < 11; ++b) {
+      float choose_mat = drand48();
+      vec3 center(a+0.9*drand48(), 0.2, b+0.9*drand48());
+      if ((center-vec3(4,0.2,0)).length() > 0.9) {
+        if (choose_mat < 0.8) { // diffuse
+          list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+        } else if (choose_mat < 0.95) { // metal
+          list[i++] = new sphere(center, 0.2,
+                                 new metal(vec3(0.5*(1+drand48()),0.5*(1+drand48()),0.5*(1+drand48())), 0.5*drand48()));
+        } else { // glass
+          list[i++] = new sphere(center, 0.2, new dialectric(1.5));
+        }
+      }
+    }
+  }
+
+  list[i++] = new sphere(vec3(0,1,0), 1.0, new dialectric(1.5));
+  list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4,0.2,0.1)));
+  list[i++] = new sphere(vec3(4,1,0), 1.0, new metal(vec3(0.7,0.6,0.5), 0.0));
+
+  return new hitable_list(list, i);
+}
+
 int main() {
   int nx = 200;
   int ny = 100;
   int ns = 100;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  hitable* world = tutorial_scene();
+  //hitable* world = tutorial_scene();
+  hitable* world = random_scene();
 
-  vec3 lookfrom(3,3,2);
-  vec3 lookat(0,0,-1);
+  vec3 lookfrom(12,1.3,3);
+  vec3 lookat(0,0.8,0);
   float dist_to_focus = (lookfrom-lookat).length();
-  float aperture = 2.0;
-  camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+  float aperture = 0.15;
+  camera cam(lookfrom, lookat, vec3(0,1,0), 18, float(nx)/float(ny), aperture, dist_to_focus);
 
   for (int j = ny-1; j >= 0; --j) {
     for (int i = 0; i < nx; ++i) {
